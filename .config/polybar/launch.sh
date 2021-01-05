4ccbfd35f7902cf330c6d9d800a1ee3a7658baf0
tiniest-train <milolathamdavis@protonmail.com>
2021-01-05 00:34:26 -0800

    Hello world, these are my dots

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+MONITOR=DP-4 polybar -c ~/.config/polybar/config.ini top &
+MONITOR=DP-4 polybar -c ~/.config/polybar/config.ini bottom &
+#MONITOR=HDMI-0 polybar -c ~/.config/polybar/config.ini top &
+#MONITOR=HDMI-0 polybar -c ~/.config/polybar/config.ini bottom &
